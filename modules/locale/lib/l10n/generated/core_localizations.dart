010b641bcb8e7a7758c3f92c0d6ce4cadc775c2c
Igor <misterrio@mail.ru>
2026-04-15 01:32:04 +0700

    Remove macos folder, fix locale files, add mocktail and fix tests

--- modules/locale/lib/l10n/generated/core_localizations.dart
+++ modules/locale/lib/l10n/generated/core_localizations.dart
@@ -2,6 +2,15 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart' as intl;
+
+import 'core_localizations_de.dart';
+import 'core_localizations_en.dart';
+import 'core_localizations_fr.dart';
+import 'core_localizations_nl.dart';
+import 'core_localizations_th.dart';
+import 'core_localizations_zh.dart';
 
 // ignore_for_file: type=lint
 
@@ -57,8 +66,7 @@ import 'package:flutter/widgets.dart';
 /// be consistent with the languages listed in the CoreLocalizations.supportedLocales
 /// property.
 abstract class CoreLocalizations {
-  CoreLocalizations(String locale)
-    : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+  CoreLocalizations(String locale) : localeName = intl.Intl.canonicalizedLocale(locale.toString());
 
   final String localeName;
 
@@ -66,8 +74,34 @@ abstract class CoreLocalizations {
     return Localizations.of<CoreLocalizations>(context, CoreLocalizations);
   }
 
+  static const LocalizationsDelegate<CoreLocalizations> delegate = _CoreLocalizationsDelegate();
+
+  /// A list of this localizations delegate along with the default localizations
+  /// delegates.
+  ///
+  /// Returns a list of localizations delegates containing this delegate along with
+  /// GlobalMaterialLocalizations.delegate, GlobalCupertinoLocalizations.delegate,
+  /// and GlobalWidgetsLocalizations.delegate.
+  ///
+  /// Additional delegates can be added by appending to this list in
+  /// MaterialApp. This list does not have to be used at all if a custom list
+  /// of delegates is preferred or required.
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
+    delegate,
+    GlobalMaterialLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+  ];
+
   /// A list of this localizations delegate's supported locales.
-  static const List<Locale> supportedLocales = <Locale>[Locale('en')];
+  static const List<Locale> supportedLocales = <Locale>[
+    Locale('de'),
+    Locale('en'),
+    Locale('fr'),
+    Locale('nl'),
+    Locale('th'),
+    Locale('zh')
+  ];
 
   /// About app dialog title
   ///
@@ -1514,4 +1548,40 @@ abstract class CoreLocalizations {
   /// In en, this message translates to:
   /// **'Failed to fetch messages'**
   String get chatMessageFetchFailure;
+}
+
+class _CoreLocalizationsDelegate extends LocalizationsDelegate<CoreLocalizations> {
+  const _CoreLocalizationsDelegate();
+
+  @override
+  Future<CoreLocalizations> load(Locale locale) {
+    return SynchronousFuture<CoreLocalizations>(lookupCoreLocalizations(locale));
+  }
+
+  @override
+  bool isSupported(Locale locale) => <String>['de', 'en', 'fr', 'nl', 'th', 'zh'].contains(locale.languageCode);
+
+  @override
+  bool shouldReload(_CoreLocalizationsDelegate old) => false;
+}
+
+CoreLocalizations lookupCoreLocalizations(Locale locale) {
+
+
+  // Lookup logic when only language code is specified.
+  switch (locale.languageCode) {
+    case 'de': return CoreLocalizationsDe();
+    case 'en': return CoreLocalizationsEn();
+    case 'fr': return CoreLocalizationsFr();
+    case 'nl': return CoreLocalizationsNl();
+    case 'th': return CoreLocalizationsTh();
+    case 'zh': return CoreLocalizationsZh();
+  }
+
+  throw FlutterError(
+    'CoreLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+    'an issue with the localizations generation tool. Please file an issue '
+    'on GitHub with a reproducible sample app and the gen-l10n configuration '
+    'that was used.'
+  );
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,41 @@
 // This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:auth/auth.dart';
 
 import 'package:abibok/main.dart';
 
+// Mock для AuthBloc
+class MockAuthBloc extends Mock implements AuthBloc {}
+
+// Fallback для mocktail
+class FakeAuthEvent extends Fake implements AuthEvent {}
+
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  late MockAuthBloc authBloc;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  setUpAll(() {
+    registerFallbackValue(FakeAuthEvent());
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+  setUp(() {
+    authBloc = MockAuthBloc();
+    when(
+      () => authBloc.stream,
+    ).thenAnswer((_) => const Stream<AuthState>.empty());
+    when(() => authBloc.state).thenReturn(const AuthState());
+  });
+
+  testWidgets('App renders without crashing', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      BlocProvider<AuthBloc>.value(value: authBloc, child: const MyApp()),
+    );
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that MaterialApp is present
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
